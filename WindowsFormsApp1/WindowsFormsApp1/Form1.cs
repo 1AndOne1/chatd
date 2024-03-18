a7d9d5a3252d381735c196764ac2028dccd4382f
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2024-03-18 23:03:04 +0500

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private TcpClient tcpClient;
+        private NetworkStream stream;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var port = 12345;
+            string ip = textBox1.Text;
+            tcpClient = new TcpClient(ip, port);
+            stream = tcpClient.GetStream();
+            textBox2.AppendText("Пользователь" + " " + ip + " " + "подключился к чату");
+            textBox2.AppendText(Environment.NewLine);
+        }
+        public async Task SendMessage(string message) 
+        {
+                try
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(message);
+                    await stream.WriteAsync(data, 0, data.Length);
+
+                    string formattedMessage = $"{message}\n";
+
+                    textBox2.AppendText(formattedMessage);
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.ScrollToCaret();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("err: " + ex.Message);
+                }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text))
+                {
+                    string message = $"{textBox1.Text}: {textBox3.Text}\n";
+                    SendMessage(message);
+                    textBox1.Clear();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string message = textBox3.Text;
+            string ip = textBox1.Text;
+            string formattedMessage = $"{message}\n";
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (i == message.Length)
+                {
+                    textBox2.AppendText(formattedMessage);
+                    textBox2.AppendText(Environment.NewLine);
+                    textBox2.ScrollToCaret();
+                }
+            }
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dasdas/das/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace client
+{
+    class Program
+    {
+        public static async Task SaveMsg(EndPoint adres, string msg)
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 12345);
+            string[] data = msg.Split(':');
+            string fileName = GenerateFilenAME(adres.ToString().Substring(0, 11), data[0]);
+            Console.WriteLine(fileName);
+            using var sw = new StreamWriter($"C:/Users/user/Desktop/das/{fileName}.txt", true);
+            await sw.WriteAsync(adres.ToString() + '_' + msg + "\n");
+            sw.Flush();
+        }
+        public static string GenerateFilenAME(string firstIp, string secondIp)
+        {
+            try
+            {
+                for (int i = 0; i < firstIp.Length; i++)
+                {
+                    int.TryParse(firstIp.Substring(i, 1), out int val);
+                    int.TryParse(secondIp.Substring(i, 1), out int val2);
+
+                    if (val > val2)
+                    {
+                        return ModIP(secondIp) + "_" + ModIP(firstIp);
+                    }
+                    else if (val2 > val)
+                    {
+                        return ModIP(firstIp) + "_" + ModIP(secondIp);
+                    }
+                }
+                
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex);
+            }
+            return null;
+        }
+        public static string ModIP(EndPoint adres, IPEndPoint ipPoint)
+        {
+            return adres.ToString().Replace('.', '_').Substring(0, 11);
+
+        }
+        public static string ModIP(string adres)
+        {
+            string modifiedAdres = adres.Replace('.', '_');
+
+            if (modifiedAdres.Length > 11)
+            {
+                modifiedAdres = modifiedAdres.Substring(0, 11);
+            }
+
+            return modifiedAdres;
+        }
+        public static async Task Main()
+        {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 12345);
+            socket.Bind(ipPoint);
+            socket.Listen();
+            while (true)
+            {
+                using var tcpClient = await socket.AcceptAsync();
+                System.Console.WriteLine("Есть подключиние");
+                byte[] bytesRead = new byte[255];
+                int count = await tcpClient.ReceiveAsync(bytesRead, SocketFlags.None);
+                string msg = Encoding.UTF8.GetString(bytesRead);
+                await Console.Out.WriteLineAsync("Принято сообщение");
+
+                if (count > 0)
+                {
+                    await SaveMsg(tcpClient.RemoteEndPoint, msg);
+                }
+            }
+        }
+    }
+}
